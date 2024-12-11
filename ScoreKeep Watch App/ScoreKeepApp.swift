f69d36f0fa03aea0cec6740a911a03bbb6d6c486
Chris Sauve <chrismsauve@gmail.com>
2024-12-10 20:26:45 -0500

    Initial Commit

---
+++ ScoreKeep Watch App/ScoreKeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeepApp.swift
+//  ScoreKeep Watch App
+//
+//  Created by Chris Sauve on 2024-12-10.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeep_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScoreKeep Watch AppTests/ScoreKeep_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeep_Watch_AppTests.swift
+//  ScoreKeep Watch AppTests
+//
+//  Created by Chris Sauve on 2024-12-10.
+//
+
+import Testing
+@testable import ScoreKeep_Watch_App
+
+struct ScoreKeep_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
